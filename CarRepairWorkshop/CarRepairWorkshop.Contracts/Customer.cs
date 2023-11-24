cba63d0dd5095eeabd590fff6189d5bdb04d5690
marton1114 <fleiszmarci@gmail.com>
2023-11-24 18:46:52 +0100

    feat: creating a model class for Customers

---
+++ CarRepairWorkshop/CarRepairWorkshop.Contracts/Customer.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarRepairWorkshop.Contracts;
+
+public class Customer
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long Id { get; set; }
+    
+    [Required]
+    public string? Address { get; set; }
+    
+    [Required]
+    [EmailAddress]
+    public string EmailAddress { get; set; }
+    
+    [Required]
+    [Phone]
+    public string PhoneNumber { get; set; }
+}
